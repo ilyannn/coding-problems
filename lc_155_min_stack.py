9b35a3fb829a6011f0027ae41832a93e51714b06
Ilya Nikokoshev <ilya.nikokoshev@gmail.com>
2021-11-30 21:42:03 +0100

    New solutions

---
+++ lc_155_min_stack.py
@@ -0,0 +1,62 @@
+""" 155. Min Stack
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+- MinStack() initializes the stack object.
+- void push(int val) pushes the element val onto the stack.
+- void pop() removes the element on the top of the stack.
+- int top() gets the top element of the stack.
+- int getMin() retrieves the minimum element in the stack.
+"""
+
+import unittest
+
+
+class MinStack:
+        """Single stack solution
+
+        Runtime: 40 ms, faster than 99.99% of Python3 online submissions for Min Stack.
+        Memory Usage: 17.9 MB, less than 93.47% of Python3 online submissions for Min Stack.
+        """
+
+        def __init__(self):
+            self.deltas = []
+            self.min = None
+
+        def push(self, val: int) -> None:
+            if not self.deltas:
+                self.min = val
+            d = val - self.min
+            self.deltas.append(d)
+            if d < 0:
+                self.min += d
+
+        def pop(self) -> None:
+            d = self.deltas.pop()
+            if d < 0:
+                self.min -= d
+            return self.min + d
+
+        def top(self) -> int:
+            return self.min + max(0, self.deltas[-1])
+
+        def getMin(self) -> int:
+            return self.min
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.solution = MinStack()
+
+    def test_1(self):
+        self.solution.push(-2)
+        self.solution.push(3)
+        self.assertEqual(self.solution.top(), 3)
+        self.assertEqual(self.solution.getMin(), -2)
+        self.assertEqual(self.solution.pop(), 3)
+        self.assertEqual(self.solution.pop(), -2)
+        self.solution.push(5)
+        self.solution.push(-1)
+        self.assertEqual(self.solution.top(), -1)
+        self.assertEqual(self.solution.getMin(), -1)
---
+++ lc_212_word_search_original.py
@@ -0,0 +1,61 @@
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        m, n = len(board), len(board[0])
+        B = n + 2
+
+        def g_enumerate():
+            """Encode board positions into small numbers"""
+            for y, row in enumerate(board):
+                for x, v in enumerate(row):
+                    yield B * y + x, v
+
+        def neighbors(p):
+            """List of neighboring cells"""
+            return (p+1, p-1, p+R, p-R)
+
+        grid = defaultdict(lambda: None) | dict(g_enumerate())
+        chars = set(grid.values())
+
+        begins = defaultdict(set)
+        for w in words:
+            # Only keep words that are possible
+            if not set(w).difference(chars):
+                begins[w[0]].add(w)
+        print(begins)
+
+        def bfs():
+            q = deque()
+            for p, v in grid:
+                for w in begins[v]:
+                    q.push((p, 0, w))
+            while q:
+                p, i, w = q.popleft()
+                for p1 in neighbors(p):
+                    if w[i] == grid[p1]:
+                        if i == len(w) - 1:
+                            yield p1, w
+                        else:
+                            q.push((p1, i+1, w))
+
+        found = list(bfs())
+        print(found)
+
+        answer = set()
+
+        def dfs(p, w, check = -1, seen=set()):
+            if check == -len(w):
+                answer.add(w)
+            if w in answer:
+                return # Don't repeat ourselves
+            seen.add(p)
+            try:
+                for p1 in neighbors(p):
+                    if p not in seen and grid[p1] == w[check]:
+                        dfs(p1, w, check - 1, seen)
+            finally:
+                seen.remove(p)
+
+        for f in found:
+            dfs(*f)
+
+        return list(answer)
