6adfda84f4c2d0f142e5b08f1e187da0b7791c97
Ilya Nikokoshev <ilya.nikokoshev@gmail.com>
2022-01-02 03:02:37 +0100

    Add comments

--- lc_0994_rotting_oranges.py
+++ lc_0994_rotting_oranges.py
@@ -5,6 +5,7 @@ https://leetcode.com/problems/rotting-oranges/
 ---
 Runtime: 44 ms, faster than 96.26% of Python3 online submissions for Rotting Oranges.
 Memory Usage: 14.4 MB, less than 39.37% of Python3 online submissions for Rotting Oranges.
+
 """
 from itertools import count
 from typing import List
@@ -13,6 +14,13 @@ import unittest
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        """Find rotting oranges using BFS
+
+        Additional optimizations:
+             - We encode board positions using small numbers of the form x + y * R
+             - Neighbors of a position p are computed as p ± 1, p ± R
+             - One selects R ≥ N + 2 since a * R - 1 must be distinguishable from b * R + N
+        """
         R = max(16, len(grid[0]) + 2)
 
         def select(z):
@@ -20,13 +28,19 @@ class Solution:
 
         fresh, rots = select(1), select(2)
 
+        # At each step keep the sets of fresh and currently rotting oranges
         for step in count():
+            # If all oranges are rotten, we are done
             if not fresh:
                 return step
+            # If no oranges can rot, we stop the process as well
             if not rots:
                 return -1
+            # If we have some fresh and some just rotten oranges, we compute the next rotting batch
             rots = fresh.intersection(n for p in rots for n in (p + 1, p - 1, p + R, p - R))
             fresh -= rots
+        # The loop eventually terminates: either the number of elements in fresh goes down,
+        # or the process is stopped during the next iteration.
 
 
 class MyTestCase(unittest.TestCase):
